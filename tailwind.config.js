279e6216a3597523ce3103105a0638407ec9251b
Andre Agostinelli <agostinelli.an@northeastern.edu>
2024-06-20 12:02:32 -0700

    Finalizing Setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
-  mode: 'jit', // Just-in-time for smaller css/faster build
-  purge: ['./public/**/*.html', './src/**/*.js'],
+  mode: 'jit', // Enable Just-in-Time mode
+  content: [
+    './public/index.html', // explicit entry pt
+    './public/**/*.html', // Include all HTML files in the public directory
+    './src/**/*.js', // Include all JavaScript files in the src directory
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'custom-blue': '#1c3d5a', // Example of custom color
+      },
+    },
   },
   variants: {},
   plugins: [],
-};
+  };
 
