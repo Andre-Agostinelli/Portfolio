7ff156420aabaf7a62131b8afceaf9a7ac814816
Andre Agostinelli <agostinelli.an@northeastern.edu>
2024-06-24 12:34:30 -0700

    Added more custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,12 +4,22 @@ module.exports = {
   content: [
     './public/index.html', // explicit entry pt
     './public/**/*.html', // Include all HTML files in the public directory
-    './src/**/*.js', // Include all JavaScript files in the src directory
+    './src/**/*.{html,js}', // Include all JavaScript files in the src directory
   ],
   theme: {
     extend: {
       colors: {
         'custom-blue': '#1c3d5a', // Example of custom color
+        transparent: 'transparent',
+        current: 'currentColor',
+        'white': '#ffffff',
+        'purple': '#3f3cbb',
+        'midnight': '#121063',
+        'metal': '#565584',
+        'tahiti': '#3ab7bf',
+        'silver': '#ecebff',
+        'bubble-gum': '#ff77e9',
+        'bermuda': '#78dcca',
       },
     },
   },
